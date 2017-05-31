ddaab99a18cb82e593b9bb4698c8438ddba5c65e
Felipe Zuniga <fzunigacar@gmail.com>
2017-05-30 22:21:56 -0400

    Added mail and post support
    Web server now serves HTML pages and process SMTP email through smtp.py

--- web_server/web_server.py
+++ web_server/web_server.py
@@ -1,6 +1,7 @@
 
 import socket
 import os
+import smtp
 from datetime import datetime
 from socket import AF_INET, SOCK_STREAM
 
@@ -12,7 +13,6 @@ class Error_304(Exception):
 Creates a new socket binded to ocelot.aul.fiu.edu
 and sarts listening for any incoming connections.
 Only 1 queued connection is allowed at a time.
-
 Takes in an integer representing the port number.
 Returns a new socket object.
 '''
@@ -23,10 +23,8 @@ def startServer(port):
     return serverSock
 
 '''
-Parses HTTP request headers into an dictionary holding
-each header and its value. The header names are the keys
-while the header values are the values.
-
+Parses HTTP request headers into a dictionary holding
+each header and its value.
 Takes in a string containing an HTTP request message.
 Returns a dicotary of headers:values.
 '''
@@ -34,15 +32,16 @@ def parseHeaders(headers):
     # Holds header to value mapping
     headersDict = {}
     # Separate each line in the request message
-    headersList = headers.split('\n')
-    print headersList
+    headersList = headers.split('\r\n')
+    #print headersList
     # For each item in the list of headers...
     for item in headersList:
         # Separate header from value
         temp = item.partition(" ")
-        print temp
         # Map header to value in a dict
         headersDict[temp[0]] = temp[2]
+    # Testing only
+    print headersDict
     return headersDict
 
 '''
@@ -55,8 +54,8 @@ Returns true if d1 comes after d2, false otherwise
 def compareDates(d1, d2):
     # Convert string dates to datetime objects
     date1 = datetime.strptime(d1, '%d %b %Y %H:%M:%S')
-    date2 = datetime.strptime(d2, '%d %b %Y %H:%M:%S ')
-    if date1 > date2:
+    date2 = datetime.strptime(d2, '%d %b %Y %H:%M:%S')
+    if date1 >= date2:
 	       return True
     else:
 	       return False
@@ -98,14 +97,39 @@ def processGET(headersMap):
     # Check for If-Modified-Since [O(1)]
     if 'If-Modified-Since:' in headersMap:
         # Get requested date, save only day, month, year and time
-        requestDate = headersMap['If-Modified-Since:'].partition(" ")[2].partition("GMT")[0]
+        requestDate = headersMap['If-Modified-Since:']
         print 'requestedDate: ' + requestDate + ' lastModified: ' + str(lastModified)
         # Check if the requested file meets the date criteria
         if not compareDates(lastModified, requestDate):
             raise Error_304("304 error")
 
     # Success
-    return '''HTTP/1.1 200 OK\nContent-Type: text/plain\nLast-Modified:'''  + str(lastModified) + '\n\n' + str(usrData)
+    return '''HTTP/1.1 200 OK\nLast-Modified:'''  + str(lastModified) + '''\nContent-Type: text/html'''  + '''\n\n''' + usrData
+
+
+'''
+Processes an HTTP Post request.
+Takes in a dictionary containing the headers mapped to their values.
+Returns an HTTP response.
+'''
+def processPost(headers,cliMsg):
+    # Check where to post data (Email)
+    if headers['POST'] == '/public_html/felipe_server/mail_server HTTP/1.1':
+        print 'Sending email.'
+        # Parse request payload
+        temp = cliMsg.split('\n')
+        emailInfo = temp[len(temp)-1].split('&')
+        print 'EmailInfo:\n', emailInfo
+        emailDict = {}
+        for value in emailInfo:
+            tempList = value.split('=')
+            emailDict[tempList[0]] = tempList[1]
+        # Send email to requested location
+        print emailDict
+        smtp.sendEmail(emailDict)
+    # Success
+    return '''HTTP/1.1 200 OK\nContent-Type: text/html'''  + '''\n\n''' + '''Email sent succesfully!'''
+
 
 def main():
     serverSocket = startServer(5070)
@@ -117,11 +141,16 @@ def main():
             # Recv returns False if the connection was closed
             if not clientMsg:
                 break
-            print clientMsg
+            print 'Request:\n'+ clientMsg
             # Get request method and headers mapped to their values
             request, headers = parseMsg(clientMsg)
             if request == 'GET':
                 HTTP_response = processGET(headers)
+                print 'Response:\n' + HTTP_response
+                clientSocket.send(HTTP_response)
+            if request == 'POST':
+                HTTP_response = processPost(headers,clientMsg)
+                print 'Response:\n' + HTTP_response
                 clientSocket.send(HTTP_response)
             # userFile = open(fName) # Throws IOError if the file was not found
             # userData = userFile.read()
